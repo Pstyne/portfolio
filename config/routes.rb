4826acb7c3325ee6590c9719023155fc9445f50e
Andrew Birt <abirt1986@gmail.com>
2020-09-27 06:25:05 -0400

    customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :works
+  resources :works, except: [:show]
+  get 'portfolio/:id', to: 'works#show', as: 'portfolio_show'
   
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
