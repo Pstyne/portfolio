2adfd4d06258d50dc1cd8038de8f8c59ef11a836
Andrew Birt <abirt1986@gmail.com>
2020-09-27 05:08:47 -0400

    implemented edit and update functionality

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -9,13 +9,27 @@ class WorksController < ApplicationController
 
   def create
     @portfolio_item = Work.new(params.require(:work).permit(:title, :subtitle, :body))
-
     if @portfolio_item.save
       flash[:success] = "Portfolio item was successfully created!"
       redirect_to works_path
     else
       flash[:danger] = "Could not create portfolio item."
       render "new"
+    end
+  end
+
+  def edit
+    @portfolio_item = Work.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Work.find(params[:id])
+    if @portfolio_item.update(params.require(:work).permit(:title, :subtitle, :body))
+      flash[:success] = "Portfolio item was successfully updated!"
+      redirect_to works_path
+    else
+      flash[:danger] = "Could not update portfolio item."
+      render "edit"
     end
   end
 end
