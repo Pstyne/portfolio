bf4025692288fbef7877771c1e08d42742d94082
Andrew Birt <abirt1986@gmail.com>
2020-09-27 04:30:04 -0400

    integrated new and create actions for portfolios

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,20 @@ class WorksController < ApplicationController
   def index
     @portfolio_items = Work.all
   end
+
+  def new
+    @portfolio_item = Work.new
+  end
+
+  def create
+    @portfolio_item = Work.new(params.require(:work).permit(:title, :subtitle, :body))
+
+    if @portfolio_item.save
+      flash[:success] = "Portfolio item was successfully created!"
+      redirect_to works_path
+    else
+      flash[:danger] = "Could not create portfolio item."
+      render "new"
+    end
+  end
 end
