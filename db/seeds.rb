86a230976eb3adcc22026b3eb991a41a907ee182
Andrew Birt <abirt1986@gmail.com>
2020-09-28 05:23:49 -0400

    implemented custom scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 topics created!"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
@@ -9,6 +17,7 @@
     Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi 
     consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, 
     vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+    topic_id: Topic.last.id
   )
 end
 
@@ -23,10 +32,23 @@ end
 
 puts "5 skills created!"
 
-9.times do |work_item|
+8.times do |work_item|
   Work.create!(
     title: "Portfolio title: #{work_item}",
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+    in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+    sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/350x200"
+  )
+end
+
+1.times do |work_item|
+  Work.create!(
+    title: "Portfolio title: #{work_item}",
+    subtitle: "React",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
     in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
