df4d568f9f4bdb37470cf717f4d45e2b3bcf95e4
Andrew Birt <abirt1986@gmail.com>
2020-09-28 06:07:44 -0400

    implemented concern for managing data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_090729) do
+ActiveRecord::Schema.define(version: 2020_09_28_095829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_09_28_090729) do
     t.integer "percent_util"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
